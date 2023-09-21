26dd47acbe30c0e729f065c22d18200d93c18483
EmmaTheDev01 <worldoffictionrw@gmail.com>
2023-09-21 18:17:20 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const key = "tCXpW7zSLoeqev2-_XiTU9yBLpnEGlGibO0sgcNzfFA";
+
+const input = document.getElementById("search_input");
+const search = document.getElementById("btn");
+const show_more = documemt.getElementById("show_more");
