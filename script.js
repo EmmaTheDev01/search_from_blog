f37d8efd02e0381c8736cfa7c7a12d619398cec2
EmmaTheDev01 <worldoffictionrw@gmail.com>
2023-09-21 18:45:52 +0300

    Initial commit

--- script.js
+++ script.js
@@ -2,4 +2,35 @@ const key = "tCXpW7zSLoeqev2-_XiTU9yBLpnEGlGibO0sgcNzfFA";
 
 const input = document.getElementById("search_input");
 const search = document.getElementById("btn");
+const output = document.getElementById("results");
 const show_more = documemt.getElementById("show_more");
+
+let input_query = "";
+let page_number = 1;
+
+ async function search_images(){
+    input_query = input.value;
+    let search_url = `https://api.unsplash.com/search/photos?page=${page_number}&query=${input_query}&client_id=${key}`;
+
+    const res = await fetch(search_url);
+    const data = await res.json();
+    const result = data.results;
+
+    if(page == 1){
+        output.innerHTML = "";
+    }
+    result.map(result => {
+        
+        const image_container = document.createElement("div");
+        image_container.classList.add("search_query");  
+        const image = document.createElement("img");
+        image.src = result.urls.small;
+        image.alt = result.alt_description;
+        image_container.appendChild(image);
+    });
+    page_number++;
+    if(page_number>1){
+        show_more.classList.remove("hidden");
+    }
+}
+
